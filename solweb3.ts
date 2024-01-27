863f0b174d496cafcf2c561319a5aee70c02bf32
Trung Do <dttr278@gmail.com>
2024-01-27 14:10:02 +0700

    improve performance

--- solweb3.ts
+++ solweb3.ts
@@ -2,6 +2,25 @@ import { Connection, LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js";
 import { RPC } from "./common";
 const connection = new Connection(RPC);
 
+/**
+ *  array limit process
+ * @param array
+ * @param callBack
+ * @param limit default 100
+ * @returns
+ */
+export const arraySliceProcess = async <T>(
+  array: T[],
+  callBack: (value: T[]) => Promise<any>,
+  limit = 100
+) => {
+  if (array.length === 0) return;
+  for (let i = 0; i < array.length; i += limit) {
+    const arrayToProcess = array.slice(i, i + limit);
+    await callBack(arrayToProcess);
+  }
+};
+
 async function retry(fn: () => Promise<any>, retries = 5) {
   try {
     return await fn();
@@ -23,41 +42,56 @@ async function getTransferTx(address: string, limit = 100, before?: string) {
     limit,
     before,
   });
-  const rs = [];
+  const rs: {
+    signature: string;
+    fromAddress: string;
+    toAddress: string;
+    amount: number;
+  }[] = [];
+  await arraySliceProcess(
+    signatures,
+    async (arr) => {
+      await Promise.all(
+        arr.map(async (sig) => {
+          const transaction = await retry(
+            async () =>
+              await connection.getParsedTransaction(sig.signature, {
+                maxSupportedTransactionVersion: 0,
+                commitment: "confirmed",
+              })
+          );
+          if (transaction && transaction.meta && !transaction.meta.err) {
+            for (const instruction of transaction.transaction.message
+              .instructions) {
+              if ("parsed" in instruction && instruction.program === "system") {
+                if (instruction.parsed.type === "transfer") {
+                  const fromAddress = instruction.parsed.info.source;
+                  const toAddress = instruction.parsed.info.destination;
+                  const amount =
+                    instruction.parsed.info.lamports / LAMPORTS_PER_SOL; // Convert lamports to SOL
 
-  for (const sig of signatures) {
-    const transaction = await retry(
-      async () =>
-        await connection.getParsedTransaction(sig.signature, {
-          maxSupportedTransactionVersion: 0,
-          commitment: "confirmed",
-        })
-    );
-    if (transaction && transaction.meta && !transaction.meta.err) {
-      for (const instruction of transaction.transaction.message.instructions) {
-        if ("parsed" in instruction && instruction.program === "system") {
-          if (instruction.parsed.type === "transfer") {
-            const fromAddress = instruction.parsed.info.source;
-            const toAddress = instruction.parsed.info.destination;
-            const amount = instruction.parsed.info.lamports / LAMPORTS_PER_SOL; // Convert lamports to SOL
+                  console.log(`Transfer from: ${fromAddress}`);
+                  console.log(`Transfer to: ${toAddress}`);
+                  console.log(`Amount: ${amount} SOL`);
 
-            console.log(`Transfer from: ${fromAddress}`);
-            console.log(`Transfer to: ${toAddress}`);
-            console.log(`Amount: ${amount} SOL`);
-
-            rs.push({
-              signature: sig.signature,
-              fromAddress,
-              toAddress,
-              amount,
-            });
+                  rs.push({
+                    signature: sig.signature,
+                    fromAddress,
+                    toAddress,
+                    amount,
+                  });
+                }
+              }
+            }
+          } else {
+            console.log("Transaction not found or failed");
           }
-        }
-      }
-    } else {
-      console.log("Transaction not found or failed");
-    }
-  }
+        })
+      );
+    },
+    10 // 10 concurrent requests
+  );
+
   return { txs: signatures, rs };
 }
 
